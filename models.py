b1ac5d2309138f8834cb9df407e14b7bd506dd70
Amirah Saharin <amirahsaharin@gmail.com>
2025-07-03 01:56:42 +0800

    Initial commit

--- models.py
+++ models.py
@@ -3,13 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'  # explicitly set table name
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    role = db.Column(db.String(20))  # organizer or attendee
+    role = db.Column(db.String(20))  # 'organizer' or 'attendee'
 
 class Event(db.Model):
+    __tablename__ = 'events'  # match what your code queries
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     date = db.Column(db.String(50))
@@ -18,6 +20,7 @@ class Event(db.Model):
     description = db.Column(db.Text)
 
 class Registration(db.Model):
+    __tablename__ = 'registrations'  # good practice
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
